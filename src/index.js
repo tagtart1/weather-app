fa7a10752ab4db50d7d4a1038cb4f0296ae8c7f0
tagtart1 <adamtbizz@gmail.com>
2022-10-11 19:34:53 -0500

    Add correct display off all required weather data

--- src/index.js
+++ src/index.js
@@ -5,17 +5,50 @@ const currentWeatherData = {};
 const form = document.querySelector("form");
 const searchBar = document.getElementById("search-city");
 
+const displayData = () => {
+  const temp = document.querySelector(".temp");
+  temp.textContent = currentWeatherData.main.temp;
+
+  const location = document.querySelector(".location");
+  location.textContent = `${currentWeatherData.name}, ${currentWeatherData.country}`;
+
+  const hiTemp = document.querySelector(".hi span:last-child");
+  hiTemp.textContent = currentWeatherData.main.temp_max;
+
+  const loTemp = document.querySelector(".lo span:last-child");
+  loTemp.textContent = currentWeatherData.main.temp_min;
+
+  const weatherName = document.querySelector(".weather-name");
+  weatherName.textContent = currentWeatherData.weather[0].main;
+
+  const weatherDetails = document.querySelector(".weather-details");
+  weatherDetails.textContent = currentWeatherData.weather[0].description;
+
+  const feelsLikeTemp = document.querySelector(".feels-like p:last-child");
+  feelsLikeTemp.textContent = currentWeatherData.main.feels_like;
+
+  const humidity = document.querySelector(".humidity p:last-child");
+  humidity.textContent = currentWeatherData.main.humidity;
+
+  const pressure = document.querySelector(".pressure p:last-child");
+  pressure.textContent = currentWeatherData.main.pressure;
+
+  const wind = document.querySelector(".wind p:last-child");
+  wind.textContent = currentWeatherData.wind;
+};
+
 const findCityWeather = (city) => {
   WeatherAPIHandler.getCityData(city).then((data) => {
     Object.assign(currentWeatherData, data);
+    displayData();
+    console.log(currentWeatherData);
   });
-  console.log(currentWeatherData);
 };
-
-console.log(currentWeatherData);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   findCityWeather(searchBar.value);
   form.reset();
 });
+
+findCityWeather("London");
--- src/weatherAPIhandler.js
+++ src/weatherAPIhandler.js
@@ -1,17 +1,84 @@
 import { pick } from "lodash";
 
 const key = "2f685353f752e350aeaea48d4539ebec";
-
+// Process the data obj for only relevent data
 const processObject = (unFilteredData) => {
+  // Picks only the properties we need
   const filteredData = pick(unFilteredData, [
     "main",
     "weather",
     "name",
     "wind",
+    "sys",
   ]);
+  // Apply any final touches to its data before we send it back
+  // Rounds the temps and adds a symbol
+  filteredData.main.temp = Math.round(filteredData.main.temp)
+    .toString()
+    .concat("°");
+
+  filteredData.main.feels_like = Math.round(filteredData.main.feels_like)
+    .toString()
+    .concat("°");
+
+  filteredData.main.temp_max = Math.round(filteredData.main.temp_max)
+    .toString()
+    .concat("°");
+
+  filteredData.main.temp_min = Math.round(filteredData.main.temp_min)
+    .toString()
+    .concat("°");
+  // Make humudity into percent
+  filteredData.main.humidity = filteredData.main.humidity
+    .toString()
+    .concat("%");
+  // Add unit to pressure
+  filteredData.main.pressure = filteredData.main.pressure
+    .toString()
+    .concat("mb");
+  // Calculate wind speed and direction
+  const deg = filteredData.wind.deg;
+  let windDirection;
+  switch (true) {
+    case (deg >= 338 || deg <= 0) && deg < 22:
+      windDirection = "N";
+      break;
+    case deg >= 22 && deg < 68:
+      windDirection = "NE";
+      break;
+    case deg >= 68 && deg < 112:
+      windDirection = "E";
+      break;
+    case deg >= 112 && deg < 158:
+      windDirection = "SE";
+      break;
+    case deg >= 158 && deg < 202:
+      windDirection = "S";
+      break;
+    case deg >= 202 && deg < 248:
+      windDirection = "SW";
+      break;
+    case deg >= 248 && deg < 292:
+      windDirection = "W";
+      break;
+    case deg >= 292 && deg < 338:
+      windDirection = "NW";
+      break;
+    default:
+      windDirection = "Error";
+  }
+
+  filteredData.wind = `${windDirection} ${Math.round(
+    filteredData.wind.speed
+  )} mph`;
+  // Extract the country name out to its own property
+  filteredData.country = filteredData.sys.country;
+  delete filteredData.sys;
+
   return filteredData;
 };
 
+// Hits API for weather data on given city
 export const getCityData = async (city) => {
   try {
     const response = await fetch(
@@ -19,6 +86,7 @@ export const getCityData = async (city) => {
       { mode: "cors" }
     );
     const weatherData = await response.json();
+    console.log(weatherData);
     return processObject(weatherData);
   } catch (error) {
     return error;
